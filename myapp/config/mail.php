9c01d7fc8f4e6368e11c96e9a2708c17b34dadd8
insert.baek@gmail.com <insert.baek@gmail.com>
2017-07-04 14:59:43 +0900

    사용자 인증

--- myapp/config/mail.php
+++ myapp/config/mail.php
@@ -55,9 +55,15 @@ return [
     |
     */
 
+    /*
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
+    ],
+    */
+    'from' => [
+        'address' => 'john@example.com',
+        'name' => 'Admin'
     ],
 
     /*

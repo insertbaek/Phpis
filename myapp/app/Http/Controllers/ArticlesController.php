525d05e500e298ebb6ec084af400634fd4f342d5
insert.baek@gmail.com <insert.baek@gmail.com>
2017-07-04 19:30:33 +0900

    입력값 유효성 검사

--- myapp/app/Http/Controllers/ArticlesController.php
+++ myapp/app/Http/Controllers/ArticlesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//use App\Http\Requests\ArticlesRequest;
+use App\Http\Requests\ArticlesRequest as asArticlesRequest;
 
 class ArticlesController extends Controller
 {
@@ -38,7 +40,8 @@ class ArticlesController extends Controller
      */
     public function create()
     {
-        return __METHOD__ . '은(는) Article 컬렉션을 만들기 위한 폼을 담은 뷰를 반환합니다.';
+        //return __METHOD__ . '은(는) Article 컬렉션을 만들기 위한 폼을 담은 뷰를 반환합니다.';
+        return view('articles.create');
     }
 
     /**
@@ -47,9 +50,49 @@ class ArticlesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    /*
     public function store(Request $request)
     {
-        return __METHOD__ . '은(는) 사용자의 입력한 폼 데이터로 새로운 Article 컬렉션을 만듭니다.';
+        //return __METHOD__ . '은(는) 사용자의 입력한 폼 데이터로 새로운 Article 컬렉션을 만듭니다.';
+        $rules = [
+            'title'     => ['required'],
+            'content'   => ['required', 'min:10'],
+        ];
+
+        $messages = [
+            'title.required' => '제목은 필수 입력 항목입니다.',
+            'content.required' => '본문은 필수 입력 항목입니다.',
+            'content.min' => '본문은 최소 :min 글자 이상이 필요합니다.',
+        ];
+
+        //*
+        $validator = \Validator::make($request->all(), $rules, $messages);
+
+        if($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+        *
+        $this->validate($request, $rules, $messages);
+
+        $article = \App\User::find(1)->articles()->create($request->all());
+
+        if(!$article) {
+            return back()->with('flash_message', '글이 저장되지 않았습니다.')->withInput();
+        }
+
+        return redirect(route('articles.index'))->with('flash_message', '작성하신 글이 저장되었습니다.');
+    }
+    */
+    //public function store(\App\Http\Requests\ArticlesRequest $request) {
+    //public function store(ArticlesRequest $request) {
+    public function store(asArticlesRequest $request) {
+        $article = \App\User::find(1)->articles()->create($request->all());
+
+        if(!$article) {
+            return back()->with('flash_message', '글이 저장되지 않았습니다.')->withInput();
+        }
+
+        return redirect(route('articles.index'))->with('flash_message', '작성하신 글이 저장되었습니다.');
     }
 
     /**

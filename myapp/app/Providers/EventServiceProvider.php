f79e29a020b805cb4714cebf2ddd1d16b9329638
insert.baek@gmail.com <insert.baek@gmail.com>
2017-07-17 16:53:12 +0900

    이벤트 시스템

--- myapp/app/Providers/EventServiceProvider.php
+++ myapp/app/Providers/EventServiceProvider.php
@@ -13,8 +13,13 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        /*
+        \App\Events\ArticlesEvent::class => [
+            \App\Listeners\ArticlesEventListener::class,
+        ],
+        */
+        \Illuminate\Auth\Events\Login::class => [
+            \App\Listeners\UsersEventListener::class
         ],
     ];
 
@@ -27,6 +32,11 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        /*
+        \Event::listen(
+            \App\Events\ArticleCreated::class,
+            \App\Listeners\ArticlesEventListener::class
+        );
+        */
     }
 }

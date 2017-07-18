9d192236d71cb41b3a9c086ba408cef964f5e57d
insert.baek@gmail.com <insert.baek@gmail.com>
2017-07-18 11:08:51 +0900

    예외 처리와 디버깅

--- myapp/app/Http/Controllers/ArticlesController.php
+++ myapp/app/Http/Controllers/ArticlesController.php
@@ -29,6 +29,8 @@ class ArticlesController extends Controller
         */
 
         $articles = \App\Article::latest()->paginate(3);
+
+        dd(view('articles.index', compact('articles'))->render());
         
         return view('articles.index', compact('articles'));
     }
@@ -107,7 +109,12 @@ class ArticlesController extends Controller
      */
     public function show($id)
     {
-        return __METHOD__ . '은(는) 다음 기본 키를 가진 Article 모델을 조회합니다.:' . $id;
+        $article = \App\Article::findOrFail($id);
+
+        //dd($article);
+
+        return $article->toArray();
+        //return __METHOD__ . '은(는) 다음 기본 키를 가진 Article 모델을 조회합니다.:' . $id;
     }
 
     /**

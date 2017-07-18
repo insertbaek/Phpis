f9a059fc527733091e01c268375cf0fdca8a39ca
insert.baek@gmail.com <insert.baek@gmail.com>
2017-07-18 17:48:47 +0900

    컴포저

--- myapp/app/Http/Controllers/ArticlesController.php
+++ myapp/app/Http/Controllers/ArticlesController.php
@@ -113,8 +113,11 @@ class ArticlesController extends Controller
 
         //dd($article);
 
-        return $article->toArray();
+        //return $article->toArray();
         //return __METHOD__ . '은(는) 다음 기본 키를 가진 Article 모델을 조회합니다.:' . $id;
+
+        debug($article->toArray());
+        return view('articles.show', compact('article'));
     }
 
     /**
